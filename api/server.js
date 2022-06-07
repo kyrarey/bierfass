4fe075936f174ddec4e94e95688cf820763319c1
alejaramos <iaramosr@gmail.com>
2022-06-07 17:23:51 -0400

    server and models II

--- api/server.js
+++ api/server.js
@@ -23,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //passport
+//new comment
 
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
